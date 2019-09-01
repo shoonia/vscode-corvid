b25194c9e35b6138f8b789f9c11345090cc5b012
Alexander Zaytsev <zaytsev126@gmail.com>
2019-09-02 00:05:55 +0300

    Terser setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 
-export default {
+const config = {
   input: './src/extension.js',
   output: {
     file: './dist/extension.js',
@@ -17,3 +18,29 @@ export default {
     include: './src/**',
   },
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    terser({
+      parse: {
+        ecma: 8,
+      },
+      compress: {
+        ecma: 8,
+        warnings: false,
+        comparisons: false,
+        inline: 2,
+        drop_console: true,
+        passes: 3,
+        unsafe_methods: true,
+      },
+      output: {
+        ecma: 8,
+        comments: false,
+        ascii_only: true,
+      },
+    }),
+  );
+}
+
+export default config;
