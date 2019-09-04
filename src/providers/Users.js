1eec9b1b29d00e319a42774714b08c0e7a66732a
Alexander Zaytsev <zaytsev126@gmail.com>
2019-09-05 00:10:25 +0300

    User details

--- src/providers/Users.js
+++ src/providers/Users.js
@@ -1,12 +1,39 @@
 import { createCompletionList, K } from './util';
 
 const list = createCompletionList([
-  { name: 'id', kind: K.Property },
-  { name: 'loggedIn', kind: K.Property },
-  { name: 'role', kind: K.Property },
-  { name: 'getEmail', kind: K.Method },
-  { name: 'getPricingPlans', kind: K.Method },
-  { name: 'getRoles', kind: K.Method },
+  {
+    name: 'id',
+    kind: K.Property,
+    detail: 'get id(): string',
+    docs: 'Gets the user\'s ID.',
+  },
+  {
+    name: 'loggedIn',
+    kind: K.Property,
+    detail: 'get loggedIn(): boolean',
+    docs: 'Indicates whether the user is logged in or not.',
+  },
+  {
+    name: 'getEmail',
+    kind: K.Method,
+    snippet: 'getEmail()$1',
+    detail: 'function getEmail(): Promise<string>',
+    docs: 'Gets the email of the current user.',
+  },
+  {
+    name: 'getPricingPlans',
+    kind: K.Method,
+    snippet: 'getPricingPlans()$1',
+    detail: 'function getPricingPlans(): Promise<Array<PricingPlan>>',
+    docs: 'Gets the user\'s member pricing plan.',
+  },
+  {
+    name: 'getRoles',
+    kind: K.Method,
+    snippet: 'getRoles()$1',
+    detail: 'function getRoles(): Promise<Array<UserRole>>',
+    docs: 'Gets the user\'s member roles.',
+  },
 ]);
 
 export default {
