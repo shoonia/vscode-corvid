6a4fcc1bb2a820fe6a0b6b1d2321f2f411be341e
Alexander Zaytsev <zaytsev126@gmail.com>
2019-09-05 01:04:38 +0300

    wixLocation details

--- src/providers/modules.js
+++ src/providers/modules.js
@@ -1,28 +1,126 @@
 import { createCompletionList, isBackend, K } from './util';
 
+const wixFetch = {
+  name: 'import { fetch, getJSON } from \'wix-fetch\'',
+  kind: K.Module,
+  snippet: '{ ${1|fetch,getJSON|} } from \'wix-fetch\';',
+  docs: 'Retrieves the specified resource from the network using HTTPS.',
+};
+
+const wixData = {
+  name: 'import wixData from \'wix-data\'',
+  kind: K.Module,
+  snippet: 'wixData from \'wix-data\';',
+  docs: 'The wix-data module contains functionality for working with data in collections.',
+};
+
 const listFronend = createCompletionList([
-  { name: 'wixBookings from \'wix-bookings\';', kind: K.Module },
-  { name: 'wixCRM from \'wix-crm\';', kind: K.Module },
-  { name: 'wixData from \'wix-data\';', kind: K.Module },
-  { name: 'wixDataset from \'wix-dataset\';', kind: K.Module },
-  { name: 'wixLocation from \'wix-location\';', kind: K.Module },
-  { name: 'wixPaidPlans from \'wix-paid-plans\';', kind: K.Module },
-  { name: 'wixPay from \'wix-pay\';', kind: K.Module },
-  { name: 'wixSite from \'wix-site\';', kind: K.Module },
-  { name: 'wixStorage from \'wix-storage\';', kind: K.Module },
-  { name: 'wixStores from \'wix-stores\';', kind: K.Module },
-  { name: 'wixUsers from \'wix-users\';', kind: K.Module },
-  { name: 'wixWindow from \'wix-window\';', kind: K.Module },
+  wixFetch,
+  wixData,
+  {
+    name: 'import wixBookings from \'wix-bookings\'',
+    kind: K.Module,
+    snippet: 'wixBookings from \'wix-bookings\';',
+    docs: 'The wix-bookings module contains functionality for working with bookings from client-side code.',
+  },
+  {
+    name: 'import wixCRM from \'wix-crm\'',
+    kind: K.Module,
+    snippet: 'wixCRM from \'wix-crm\';',
+    docs: 'The wix-crm module contains functionality for working with your site\'s contacts from client-side code.',
+  },
+  {
+    name: 'import wixLocation from \'wix-location\'',
+    kind: K.Module,
+    snippet: 'wixLocation from \'wix-location\';',
+    docs: 'The wix-location module contains functionality for getting information about the URL of the current page and for navigating to other pages.',
+    // docs: '',
+  },
+  {
+    name: 'import wixPaidPlans from \'wix-paid-plans\'',
+    kind: K.Module,
+    snippet: 'wixPaidPlans from \'wix-paid-plans\';',
+    // detail: '',
+    docs: 'The wix-paid-plans module contains functionality for working with paid plans from client-side code.',
+  },
+  {
+    name: 'wixPay from \'wix-pay\'',
+    kind: K.Module,
+    // snippet: '',
+    // detail: '',
+    // docs: '',
+  },
+  {
+    name: 'wixSite from \'wix-site\'',
+    kind: K.Module,
+    // snippet: '',
+    // detail: '',
+    // docs: '',
+  },
+  {
+    name: '{ local, session, memory } from \'wix-storage\'',
+    kind: K.Module,
+    snippet: '{ ${1|local,session,memory|} } from \'wix-storage\';',
+    docs: 'The wix-storage module contains functionality for the persistent storage of key/value data in the site visitor\'s browser.',
+  },
+  {
+    name: 'wixStores from \'wix-stores\'',
+    kind: K.Module,
+    // snippet: '',
+    // detail: '',
+    // docs: '',
+  },
+  {
+    name: 'wixUsers from \'wix-users\'',
+    kind: K.Module,
+    // snippet: '',
+    // detail: '',
+    // docs: '',
+  },
+  {
+    name: 'wixWindow from \'wix-window\'',
+    kind: K.Module,
+    // snippet: '',
+    // detail: '',
+    // docs: '',
+  },
 ]);
 
 const listBackend = createCompletionList([
-  { name: 'wixData from \'wix-data\';', kind: K.Module }, // Duplication. Find a better way
-  { name: 'wixCRM from \'wix-crm-backend\';', kind: K.Module },
-  { name: 'wixMarketing from \'wix-marketing-backend\';', kind: K.Module },
-  { name: 'wixPay from \'wix-pay-backend\';', kind: K.Module },
-  { name: 'wixSite from \'wix-site-backend\';', kind: K.Module },
-  { name: 'wixStores from \'wix-stores-backend\';', kind: K.Module },
-  { name: 'wixUsers from \'wix-users-backend\';', kind: K.Module },
+  wixFetch,
+  wixData,
+  {
+    name: 'import wixBilling from \'wix-billing-backend\'',
+    kind: K.Module,
+    snippet: 'wixBilling from \'wix-billing-backend\';',
+    docs: 'The wix-billing-backend module contains functionality for working with billing features, such as price quotes and invoices.',
+  },
+  {
+    name: 'wixCRM from \'wix-crm-backend\'',
+    kind: K.Module,
+    snippet: 'wixCRM from \'wix-crm-backend\';',
+    docs: 'The wix-crm-backend module contains functionality for working with your site\'s contacts from backend code.',
+  },
+  {
+    name: 'wixMarketing from \'wix-marketing-backend\'',
+    kind: K.Module,
+  },
+  {
+    name: 'wixPay from \'wix-pay-backend\'',
+    kind: K.Module,
+  },
+  {
+    name: 'wixSite from \'wix-site-backend\'',
+    kind: K.Module,
+  },
+  {
+    name: 'wixStores from \'wix-stores-backend\'',
+    kind: K.Module,
+  },
+  {
+    name: 'wixUsers from \'wix-users-backend\'',
+    kind: K.Module,
+  },
 ]);
 
 export default {
