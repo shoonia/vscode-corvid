aea0a727f446abd06d7d16a07262544b85142218
shoonia <zaytsev126@gmail.com>
2019-09-05 16:21:30 +0300

    wixStorage details

--- src/providers/wixStorage.js
+++ src/providers/wixStorage.js
@@ -1,10 +1,34 @@
 import { createCompletionList, K } from './util';
 
 const list = createCompletionList([
-  { name: 'getItem', kind: K.Method },
-  { name: 'setItem', kind: K.Method },
-  { name: 'removeItem', kind: K.Method },
-  { name: 'clear', kind: K.Method },
+  {
+    name: 'getItem',
+    kind: K.Method,
+    snippet: 'getItem(${1:key});',
+    detail: 'function getItem(key: string): string',
+    docs: 'Gets an item from local, session, or memory storage.',
+  },
+  {
+    name: 'setItem',
+    kind: K.Method,
+    snippet: 'setItem(${1:key}, ${2:value});',
+    detail: 'function setItem(key: string, value: string): void',
+    docs: 'Stores an item in local, session, or memory storage.',
+  },
+  {
+    name: 'removeItem',
+    kind: K.Method,
+    snippet: 'removeItem(${1:key});',
+    detail: 'function removeItem(key: string): void',
+    docs: 'Removes an item from local, session, or memory storage.',
+  },
+  {
+    name: 'clear',
+    kind: K.Method,
+    snippet: 'clear();',
+    detail: 'function clear(): void',
+    docs: 'Removes all items from local, session, or memory storage.',
+  },
 ]);
 
 export default {
