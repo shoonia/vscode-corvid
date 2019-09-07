156b0bcd75da5e7a2f3172b5bda87c21d134c21f
Alexander Zaytsev <zaytsev126@gmail.com>
2019-09-07 14:54:04 +0300

    wixBookings details

--- src/providers/wixBookings.js
+++ src/providers/wixBookings.js
@@ -1,8 +1,20 @@
 import { createCompletionList, K } from './util';
 
 const list = createCompletionList([
-  { name: 'checkoutBooking', kind: K.Method },
-  { name: 'getServiceAvailability', kind: K.Method },
+  {
+    name: 'checkoutBooking',
+    kind: K.Method,
+    snippet: 'checkoutBooking(${1:bookingInfo})',
+    detail: 'function checkoutBooking(bookingInfo: BookingInfo, [options: PaymentOptions]): Promise<BookingResult>',
+    docs: 'Books a service and processes payment for the service.',
+  },
+  {
+    name: 'getServiceAvailability',
+    kind: K.Method,
+    snippet: 'getServiceAvailability(${1:serviceId})',
+    detail: 'function getServiceAvailability(serviceId: string, [options: AvailabilityOptions]): Promise<ServiceAvailability>',
+    docs: 'Gets the available slots for a specific service.',
+  },
 ]);
 
 export default {
