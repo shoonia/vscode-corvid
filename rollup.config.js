f97137cfc2ef1565858e649d9bfc252e35769bf2
shoonia <alexanderz@wix.com>
2022-01-05 21:13:29 +0200

    Remove code minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,7 @@
-/* eslint-env node */
-import { terser } from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import babel from '@rollup/plugin-babel';
 
-const isProd = !process.env.ROLLUP_WATCH;
 const extensions = ['.ts'];
 
 export default {
@@ -40,30 +37,5 @@ export default {
       extensions,
     }),
     json(),
-    isProd && terser({
-      ecma: 2020,
-      module: true,
-      toplevel: true,
-      parse: {
-        ecma: 2020,
-      },
-      compress: {
-        ecma: 2020,
-        comparisons: false,
-        inline: 2,
-        drop_console: true,
-        passes: 3,
-        unsafe_methods: true,
-        module: true,
-        toplevel: true,
-        pure_getters: true,
-        unsafe: true,
-        unsafe_math: true,
-      },
-      format: {
-        ecma: 2020,
-        comments: false,
-      },
-    }),
   ],
 };
